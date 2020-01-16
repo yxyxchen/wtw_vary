14329ce84321c63a41f53ec6c77eb5abc182aa9a
yxyxchen <yxyxchen@bu.edu>
2020-01-16 18:09:29 -0500

    daily

--- subFxs/expParas.R
+++ subFxs/expParas.R
@@ -8,7 +8,7 @@ tokenValue = c(-1, 8)
 # optimal waiting thresholds, unit = sec
 optimWaitThresholds = list(HP = 30, LP = 5) # roughly estimations from Joe's grant. 
 # optimal reward rates
-HP = mean(tokenValue) / (30 + iti)
+HP = mean(tokenValue) / (30 + iti) # this is obviously wrong. We know what 
 LP = mean(tokenValue) / (5 + iti)
 optimRewardRates = list(HP = HP, LP = LP) 
 # analyses parameters
